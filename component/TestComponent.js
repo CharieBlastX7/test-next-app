c65ff5ca963c4fb0373ac069f85017d403177cbe
CharieBlastX7 <61608668+CharieBlastX7@users.noreply.github.com>
2021-10-07 16:31:36 +0530

    Test next app init

---
+++ component/TestComponent.js
@@ -0,0 +1,10 @@
+import test from "../styles/Test.module.css";
+
+export default function TestComponent() {
+  return (
+    <div>
+      <h1 className={test.sampleclassName}>Test Component</h1>
+      <p>This is a test component</p>
+    </div>
+  );
+}
